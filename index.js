4369ac2e52f02439a0c4ef69151f9cff2038fb20
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-20 14:53:07 +0200

    Table backgroundcolor change fix

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ function szamit(){
     document.getElementById("IdeaOut").innerHTML = Idea.toFixed(2);
     document.getElementById("min").innerHTML = min.toFixed(2);
     document.getElementById("max").innerHTML = max.toFixed(2);
+    if (BMI<=18.5){
+        document.getElementById("bmi1").style.backgroundColor = "orange";
+    }else if (BMI<=24.9){
+        document.getElementById("bmi2").style.backgroundColor = "orange";
+    }else if (BMI<=29.9){
+        document.getElementById("bmi3").style.backgroundColor = "orange";
+    }else if (BMI<=34.9){
+        document.getElementById("bmi4").style.backgroundColor = "orange";;
+    }else if (BMI<=39.9){
+        document.getElementById("bmi5").style.backgroundColor = "orange";
+    }else if (BMI>=40){
+        document.getElementById("bmi6").style.backgroundColor = "orange";
+    }
 }
 function init(){
     document.getElementById("szamitas").addEventListener('click', szamit);
