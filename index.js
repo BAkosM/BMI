7a920546275462723d0717d10f61a00312e0e9b3
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-20 14:16:50 +0200

    Bootstrap and Output Fixes

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ function szamit(){
     let cm = parseFloat(document.getElementById("mag").value);
     let kg = parseFloat(document.getElementById("sul").value);
     let BMI = kg / (cm / 100) ** 2;
+    let min = parseFloat((18.5*(cm/100)**2).toFixed(1));
+    let max = parseFloat((24.9*(cm/100)**2).toFixed(1));
+    let Idea = parseFloat(((min+max)/2));
     document.getElementById("BMIout").innerHTML = BMI.toFixed(2);
+    document.getElementById("IdeaOut").innerHTML = Idea.toFixed(2);
+    document.getElementById("min").innerHTML = min.toFixed(2);
+    document.getElementById("max").innerHTML = max.toFixed(2);
 }
 function init(){
     document.getElementById("szamitas").addEventListener('click', szamit);
